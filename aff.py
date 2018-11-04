c058182201a0d7b8eff4cec73f7d5f52a06df3b0
maxcian <max.cian@cellopoint.com>
2018-11-04 06:06:19 +0000

    add aff.py

---
+++ aff.py
@@ -0,0 +1,17 @@
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return "<User {}> {}".format(self.username, self.email)
+
+
+class AntiFruitFruad(object):
+    pass
