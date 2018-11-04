d27e7b47aa337fce24200ed620a3b87832e4501c
maxcian <max.cian@cellopoint.com>
2018-11-04 07:12:51 +0000

    fix bug

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     received = event.message.text
-    ret = aff_handler.process(received)
+    ret = aff_handler.processText(received)
     message = TextSendMessage(text=ret)
     line_bot_api.reply_message(event.reply_token, message)
 
