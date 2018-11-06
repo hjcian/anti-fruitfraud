4e6cabeaa3126f821151701dc4c3096ca3fffa2a
maxcian <max.cian@cellopoint.com>
2018-11-06 14:45:31 +0000

    refine UX

--- aff.py
+++ aff.py
@@ -6,74 +6,49 @@ class AntiFruitFruad(object):
     def __init__(self, db):
         self.db = db
         self.db.create_all()
-        # self.locations = set()
-        # self.items = set()
         self.locations = set(map(lambda r: r.loc, Record.query.with_entities(Record.loc)))
         self.items = set(map(lambda r: r.name, Record.query.with_entities(Record.name)))
+        self.cmd = {
+            "show": self._displayRecord,
+            "clear": self._clear,
+            "item": lambda: "\n".join(self.items),
+            "location": lambda: "\n".join(self.locations),
+            "test": self._test,
+        }
 
+    def processText(self, text):
+        text = text.lower()
+        self.showText(text)
+        words = text.split(" ")
+        size = len(words)
+        if size == 1:
+            ret = "Not found {} in developer command and item list".format(words[0])
+            if words[0] in self.cmd: # developer command
+                ret = self.cmd.get(words[0], self._usage)()
+            elif words[0] in self.items: # find item
+                ret = self._select_by_name(words[0])
+            return ret
+        elif size in (3, 4):
+            return self.add(words)
+        else:
+            return self._usage()
 
     def _usage(self):
         return "Show usage"
 
-    def debug(self, actions):
-        if len(actions):
-            a = actions[0].strip()
-            action = {
-                "show": self._displayRecord,
-                "clear": self._clear,
-                "test": self._test,
-            }
-            ret = action.get(a, self._usage)()
-            return ret
-        else:
-            return self._usage()
-
     def add(self, actions):
-        if len(actions) >= 3:
-            r = Record(
-                time=datetime.utcnow() + timedelta(hours=8),
-                loc=actions[0],
-                name=actions[1], 
-                price=actions[2], 
-                unit=actions[3] if len(actions) > 3 else "{}".format("個")
-                )
-            self.db.session.add(r)
-            self.db.session.commit()
-            self.locations.add(actions[0])
-            self.items.add(actions[1])
-            return "{} {}({}/{})/{} [id:{}]".format(r.time.strftime("%Y-%m-%d %H:%M"), r.name, r.price, r.unit, r.loc, r.id, )
-        else:
-            return self._usage()
-
-    def show(self, actions):
-        if len(actions):
-            a = actions[0].strip()
-            action = {
-                "name": self._select_name,
-            }
-            ret = action.get(a, self._usage)()
-            return ret
-        else:
-            return self._usage()
-
-    def processText(self, text):
-        # self._init()
-        text = text.lower()
-        self.showText(text)
-        key, *actions = text.split(" ")
-        key = key.strip()
-        if key in self.items:
-            # directly search item
-            ret = self._select_by_name(key)
-            return ret
-        else:
-            commands = {
-                "debug": self.debug,
-                "add": self.add,
-                "show": self.show,
-            }
-            ret = commands.get(key, lambda dump: self._usage())(actions)
-            return ret
+        r = Record(
+            time=datetime.utcnow() + timedelta(hours=8),
+            loc=actions[0],
+            name=actions[1], 
+            price=actions[2], 
+            unit=actions[3] if len(actions) > 3 else "{}".format("個")
+            )
+        self.db.session.add(r)
+        self.db.session.commit()
+        self.locations.add(actions[0])
+        self.items.add(actions[1])
+        return "[OK] {} {}({}/{})/{} [id:{}]".format(r.time.strftime("%Y-%m-%d %H:%M"), r.name, r.price, r.unit, r.loc, r.id, )
 
     def _displayRecord(self):
         return "[develop]\n{}".format(Record.query.all())
@@ -86,23 +61,9 @@ class AntiFruitFruad(object):
     def _test(self):
         return "[develop]\n123456789A123456789B123456789C123456789D123456789E"
 
-    def _select_name(self):
-        results = Record.query.with_entities(Record.name)
-        results = set(map(lambda r: r.name, results))
-        return "\n".join(results)
-
     def _select_by_name(self, name):
         results = Record.query.with_entities(Record).filter(Record.name==name).order_by(Record.time.desc()).slice(0, 5)
-        for i in results:
-            print(i)
-        return "\n".join(map(lambda r: "[{}] {} {} ({}/{})".format(r.time, r.loc, r.name, r.price, r.unit), results))
+        return "\n".join(map(lambda r: "[{}] {} ({}/{}) [id:{}]".format(r.time.strftime("%Y-%m-%d %H:%M"), r.loc, r.price, r.unit, r.id), results))
 
     def showText(self, text):
         print("Received: {}".format(text))
-
-    def _init(self):
-        if not self.locations:
-            self.locations = set(map(lambda r: r.loc, Record.query.with_entities(Record.loc)))
-        if not self.items:
-            self.items = set(map(lambda r: r.name, Record.query.with_entities(Record.name)))
-
--- app.py
+++ app.py
@@ -11,8 +11,7 @@ from linebot.exceptions import (
 from linebot.models import *
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
 from aff import AntiFruitFruad
@@ -22,6 +21,16 @@ aff_handler = AntiFruitFruad(db)
 line_bot_api = LineBotApi('rYeJ0hEeOWbGCUHgkm04LUUz9LkHGKxHFbKl/6qFpLSvcnBwAPwNvGIpkMw2FcOlk4/8fVpAe/yNsPEmysEpDb0wyAxp1M+GCSIcBCZG/lFkPbRsus2JvF454W8hUSbO1nEc9ar8fxfZGQJqknUMaAdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('116c10a4943bedf7211cabc85aec1da2')
+
+import json
+@app.route('/dev/', methods=['POST'])
+def show_user_profile():
+    body = request.get_data(as_text=True)
+    data = json.loads(body)
+    text = data['text']
+    ret = aff_handler.processText(text)
+    return "**********\n{}\n**********\n".format(ret)
+    
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -46,7 +55,6 @@ def handle_message(event):
     message = TextSendMessage(text=ret)
     line_bot_api.reply_message(event.reply_token, message)
 
-import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
